30103b763f4d34be18ef2bda8801eb287ed566a1
Javier Calatrava <javiercalatrava@capitole-consulting.com>
2021-09-26 11:59:00 +0200

    Add xcode SwiftUI blank app project.

---
+++ Coordinators-SwiftUI/Coordinators_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coordinators_SwiftUIApp.swift
+//  Coordinators-SwiftUI
+//
+//  Created by Javier Calatrava on 26/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Coordinators_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
